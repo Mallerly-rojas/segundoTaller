81b3df68285dc4cf2092304715419b94ebf6cc87
unknown <Mallerly Rojas Cardenas>
2022-06-18 09:36:15 -0500

    se realiza la multiplicacion necesaria para el total solicitado

--- src/PesosAbolibares.java
+++ src/PesosAbolibares.java
@@ -1,5 +1,16 @@
+import javax.swing.*;
+
 public class PesosAbolibares {
     public static void main(String[] args) {
+        double pesos= 0;
+        double bolivares= 0.0014;
+        double total=0;
+
+        pesos=Double.parseDouble(JOptionPane.showInputDialog(" ingrese una cantidad en pesos"));
+
+        total= pesos * bolivares;
+
+        JOptionPane.showMessageDialog(null, pesos  +" pesos en bolivares son: " + total);
 
     }
 
